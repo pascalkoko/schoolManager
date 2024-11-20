aef0f2479faa4facb0ee3bddb82817a0a3756d10
dev@pascal <pascalkokobisa@gmail.com>
2024-11-20 13:21:25 +0200

    delib function

--- src/main/java/dev/java/schoolManager/deliberation/DelibService.java
+++ src/main/java/dev/java/schoolManager/deliberation/DelibService.java
@@ -1,38 +1,27 @@
 package dev.java.schoolManager.deliberation;
 
-import java.util.Iterator;
-import java.util.List;
-
 import dev.java.schoolManager.Etudiant;
 
 public class DelibService {
 
 	public double delibererEtudiant (Etudiant e) {
 		
-		
+		int totalGeneral = 0;
+		double  totalObtenu = 0.0;
+		Double pourcentage = null;
 		System.out.println(" \n\n Déliberation de l'etudiant " + e.getMatricule() +"  " + e.getNomComplet() 
 		+"\n points obtenus\n\n");
 		 
 		e.getPromotion().getCours().forEach(x ->{
-			int totalGeneral = 0;
-			totalGeneral += x.getCredit() * 20;
-			System.out.println(x.getIntituleCours() + " : " + x.getCote() + "    credit " + x.getCredit() + "\n");
+//			totalGeneral = 0;
+//			totalObtenu = 0.0;
+//			totalGeneral += x.getCredit() * 20;
+//			System.out.println(x.getIntituleCours() + " : " + x.getCote() + "    credit " + x.getCredit() + "\n");
+//			
+//			totalObtenu = totalObtenu +( x.getCote() * x.getCredit()); 
 		});
 		
-		for (int i = 0; i < e.getPromotion().getCours().size(); i++) {
-			
-			int totalObtenu = 0;
-			
-			e.getPromotion().getCours().forEach(x ->{
-				int totalGeneral = 0;
-				totalGeneral += x.getCredit() * 20;
-				System.out.println(x.getIntituleCours() + " : " + x.getCote() + "    credit " + x.getCredit() + "\n");
-			});
-			
-		}
-		
-		
-		return 0;
+		return (totalObtenu *100)/totalGeneral;
 		
 	}
 }
